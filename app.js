53bd6346c88b195d3fcc62a55f51f5c1b466dc20
Jaewon <jungjaedev@gmail.com>
2022-10-22 21:05:02 +0900

    Add: import Image function

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const body = document.body;
 const preview = document.querySelector('.preview');
 const solidBtn = document.querySelector('.background__solid');
 const gradientBtn = document.querySelector('.background__gradient');
+const importImageBtn = document.querySelector('.background__import_image');
 const backgroundBtns = document.querySelector('.background_btns').children;
 
 const createRGB = () => {
@@ -53,10 +54,32 @@ const changeToGradient = () => {
   });
   gradientBtn.classList.add('selected');
 
-  preview.style.background = '';
-  body.style.background = '';
   preview.style.background = `linear-gradient(to bottom, #${rgb1}, #${rgb2})`;
   body.style.background = `linear-gradient(to bottom, #${rgb1}, #${rgb2})`;
 };
 
 gradientBtn.addEventListener('click', changeToGradient);
+
+// background import image
+const importImage = function () {
+  const regex =
+    /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+  let imgUrl = prompt('이미지 주소를 입력하세요 😇');
+  if (imgUrl === null) return;
+  if (!imgUrl.match(regex)) {
+    alert('올바르지 않은 URL입니다 😨');
+    return;
+  }
+
+  body.style.background = preview.style.background = `url('${imgUrl}')`;
+  body.style.backgroundSize = preview.style.backgroundSize = 'cover';
+  body.style.backgroundRepeat = preview.style.backgroundRepeat = 'no-repeat';
+  body.style.backgroundPosition = preview.style.backgroundPosition = 'center';
+
+  [...backgroundBtns].forEach(e => {
+    e.classList.remove('selected');
+  });
+  importImageBtn.classList.add('selected');
+};
+
+importImageBtn.addEventListener('click', importImage);
